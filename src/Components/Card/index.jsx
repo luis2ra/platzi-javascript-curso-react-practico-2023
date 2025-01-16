db521ff9820d410299e689c89041b803f9b07454
Luis Altuve <laltuve@ext.sebbusiness.com>
2025-01-16 09:26:55 -0500

    clase-25: updated packages

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -24,7 +24,7 @@ const Card = (data) => {
     if (isInCart) {
       return (
         <div
-          className='absolute top-0 right-0 flex justify-center items-center bg-black w-6 h-6 rounded-full m-2 p-1'>
+          className='absolute top-0 right-0 flex justify-center items-center bg-green-600 w-6 h-6 rounded-full m-2 p-1'>
           <CheckIcon className='h-6 w-6 text-white'></CheckIcon>
         </div>
       )
